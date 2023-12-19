be89893f9657120b7b01ebdd728248f772e382e2
Vishal Ghosh <avatarvishal1551@gmail.com>
2023-12-19 15:29:12 +0530

    Info page UI done

--- lib/Screens/(2)infoPage.dart
+++ lib/Screens/(2)infoPage.dart
@@ -26,7 +26,7 @@ class _InfoPageState extends State<InfoPage> {
           padding: const EdgeInsets.only(left: 2, right: 2),
           child: Container(
             height: 700,
-            // color: Colors.amber,
+            color: const Color.fromARGB(255, 24, 24, 24),
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: <Widget>[
@@ -44,9 +44,10 @@ class _InfoPageState extends State<InfoPage> {
                           const SizedBox(
                             height: 10,
                           ),
-                          Text(infos[index]
-                              .expandableItems[itemIndex]
-                              .description),
+                          Text(
+                            infos[index].expandableItems[itemIndex].description,
+                            style: const TextStyle(color: Colors.black),
+                          ),
                           const SizedBox(
                             height: 10,
                           )
@@ -68,7 +69,18 @@ class _InfoPageState extends State<InfoPage> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        const _elevatedButton(),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 24, 24, 24),
+              elevation: 0),
+          child: const Icon(
+            Icons.chevron_left,
+            color: Colors.white,
+          ),
+        ),
         Expanded(
           child: Align(
             alignment: Alignment.center,
@@ -86,78 +98,55 @@ class _InfoPageState extends State<InfoPage> {
   Widget build(BuildContext context) {
     _getInitialInfo();
     return Scaffold(
-      body: Container(
-        // color: Colors.grey.withOpacity(0.15),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 1,
-            mainAxisSpacing: 1,
-          ),
-          itemBuilder: ((context, index) {
-            return InkWell(
-              onTap: () {
-                _showGridItemBottomSheet(context, index);
-              },
-              child: GridTile(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        border: Border.all(
-                          color: Colors.grey.withOpacity(0.5),
-                          width: 1,
-                        )),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        SizedBox(
-                          height: 70,
-                          width: 70,
-                          child: SvgPicture.asset(
-                            infos[index].iconButton,
-                          ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 1,
+          mainAxisSpacing: 1,
+        ),
+        itemBuilder: ((context, index) {
+          return InkWell(
+            onTap: () {
+              _showGridItemBottomSheet(context, index);
+            },
+            child: GridTile(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 75, 73, 73)
+                          .withOpacity(0.5),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 27, 25, 25),
+                        width: 1,
+                      )),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: SvgPicture.asset(
+                          infos[index].iconButton,
                         ),
-                        Text(
-                          infos[index].text,
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 7, 68, 172),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
+                      ),
+                      Text(
+                        infos[index].text,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            );
-          }),
-          itemCount: infos.length,
-        ),
-      ),
-    );
-  }
-}
-
-class _elevatedButton extends StatelessWidget {
-  const _elevatedButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.of(context).pop();
-      },
-      style:
-          ElevatedButton.styleFrom(backgroundColor: Colors.white, elevation: 0),
-      child: const Icon(
-        Icons.chevron_left,
-        color: Colors.black,
+            ),
+          );
+        }),
+        itemCount: infos.length,
       ),
     );
   }
